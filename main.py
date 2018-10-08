05d495520271d1067168b6b2a87ffc1b9edb4543
Taylor <gilmourguru26@gmail.com>
2018-10-08 08:06:52 -0500

    wrote code for sampling 2 movies from a list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -7,23 +8,35 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 @app.route("/")
 def index():
     # choose a movie by invoking our new function
-    movie = get_random_movie()
+
+    flicks = get_random_movie()
+    
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
-    content += "<li>" + movie + "</li>"
+    content += "<li>" + flicks[0] + "</li>"
     content += "</ul>"
+    
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    movie2 = get_random_movie()
+    
+    content += "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + flicks[1] + "</li>"
+    content += "</ul>"
 
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
-    # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
-
+    movies = ["The Big Lebowski", "Office Space", "Idiocracy", 
+    "The Wall", "National Lampoon's: Christmas Vaction"]
+    picks = random.sample(movies, 2)
+    
+    #return movies[pick]
+    return picks
 
 app.run()
